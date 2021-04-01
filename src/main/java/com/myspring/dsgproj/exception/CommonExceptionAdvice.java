b642ae7a60d57dc02dc241506b3e627dfaaa432f
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-04-01 09:30:12 +0900

    404에러 페이지 추가

--- src/main/java/com/myspring/dsgproj/exception/CommonExceptionAdvice.java
+++ src/main/java/com/myspring/dsgproj/exception/CommonExceptionAdvice.java
@@ -2,31 +2,18 @@ package com.myspring.dsgproj.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-@Controller
+// 버전 4로 맞춰야 @ControllerAdvice, NoHandlerFoundException
+@ControllerAdvice
 public class CommonExceptionAdvice {
 
 	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
-	
-	
-	@ExceptionHandler(ArithmeticException.class)	// ArithmeticException에 대해서 호출됨.
-	public String arithmeticException(Exception ex, Model model) {
-		logger.info("Exception....."+ex.getMessage());
-		model.addAttribute("exception", ex);
-		logger.info(model.toString());
-		return "/error/error";
-	}
-	
-	@ExceptionHandler(NullPointerException.class)	// NullPointerException에 대해서 호출됨.
-	public String nullPointerException(Exception ex, Model model) {
-		logger.info("Exception....."+ex.getMessage());
-		model.addAttribute("exception", ex);
-		logger.info(model.toString());
-		return "/error/error";
-	}
 	
 	@ExceptionHandler(Exception.class)	// 모든 Exception에 대해서 호출됨.
 	public String except(Exception ex, Model model) {
@@ -34,5 +21,30 @@ public class CommonExceptionAdvice {
 		model.addAttribute("exception", ex);
 		logger.info(model.toString());
 		return "error/error";
+	}
+	
+	@ExceptionHandler(ArithmeticException.class)	// ArithmeticException에 대해서 호출됨.
+	public String arithmeticException(Exception ex, Model model) {
+		logger.info("Exception....."+ex.getMessage());
+		model.addAttribute("exception", ex);
+		logger.info(model.toString());
+		return "error/error";
+	}
+	
+	@ExceptionHandler(NullPointerException.class)	// NullPointerException에 대해서 호출됨.
+	public String nullPointerException(Exception ex, Model model) {
+		logger.info("Exception....."+ex.getMessage());
+		model.addAttribute("exception", ex);
+		logger.info(model.toString());
+		return "error/error";
+	}
+	
+
+//	404 에러 페이지, 버전 4로 맞춰야
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException ex) {
+
+		return "error/custom404";
 	}
 }
